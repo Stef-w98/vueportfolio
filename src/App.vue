f678850db607fc2529355936f27614ebc46f6e44
stef-w98 <stef.wouters22@gmail.com>
2022-05-14 20:53:45 +0200

    logo test 10

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <div class="navleft">
-      <img id="logo" src="./assets/logowhite.png" alt="SWicon">
+      <img id="logo" src="assets/favicon.png" alt="SWicon">
       <h1>Stef Wouters</h1>
     </div>
     <div class="links">
